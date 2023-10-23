ee23cf6e2b9b3c90c7d7710cafdb893dc8c6c5e0
nastena122 <47711947+nastena122@users.noreply.github.com>
2023-10-23 16:48:22 +0300

    Update ConsoleApplicationTest.cpp

--- ConsoleApplicationTest/ConsoleApplicationTest.cpp
+++ ConsoleApplicationTest/ConsoleApplicationTest.cpp
@@ -1,20 +1,51 @@
-﻿// ConsoleApplicationTest.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+// Функция для вычисления скользящего среднего
+template <typename T>
+vector<T> simple_moving_average(const vector<T>& data, int window_size) {
+    vector<T> moving_averages;
+    for (int i = 0; i < data.size() - window_size + 1; ++i) {
+        T sum = 0;
+        for (int j = i; j < i + window_size; ++j) {
+            sum += data[j];
+        }
+        moving_averages.push_back(sum / window_size);
+    }
+    return moving_averages;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main() {
+    // Генерация тестовых данных
+    vector<double> data;
+    for (int i = 0; i < 1000000; ++i) {
+        data.push_back(static_cast<double>(i));
+    }
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    // Замер времени выполнения для разных длин окна и типов данных
+    vector<int> window_sizes = { 4, 8, 16, 32, 64, 128 };
+
+    for (int window_size : window_sizes) {
+        auto start_time = chrono::high_resolution_clock::now();
+        vector<double> result = simple_moving_average(data, window_size);
+        auto end_time = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+        cout << "Window size " << window_size << ": " << duration.count() << " ms" << endl;
+
+        // Сохранение результатов в CSV-файл
+        ofstream output_file("moving_average_results.csv");
+        output_file << "Data, Moving Average" << endl;
+        for (int i = 0; i < result.size(); ++i) {
+            output_file << data[i] << "," << result[i] << endl;
+        }
+        output_file.close();
+    }
+
+    cout << "Saved in moving_average_results.csv" << endl;
+
+    return 0;
+}
